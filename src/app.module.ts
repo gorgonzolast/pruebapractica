a1a8a5317c54d78ff855f9d85ca540f90afaef75
davidvaldh <ds.valderrama@uniandes.edu.co>
2023-11-19 18:14:26 -0500

    Feat: All necessary modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AirlineModule } from './airline/airline.module';
+import { AirportModule } from './airport/airport.module';
+import { AirportAirlineModule } from './airport-airline/airport-airline.module';
+import { AirlineAirportModule } from './airline-airport/airline-airport.module';
 
 @Module({
-  imports: [],
+  imports: [AirlineModule, AirportModule, AirportAirlineModule, AirlineAirportModule],
   controllers: [AppController],
   providers: [AppService],
 })
